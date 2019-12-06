7ae53aebf7b800cb258114878f086ffc235a98f4
edlingao <sonido244@gmail.com>
2019-12-05 18:09:00 -0600

    feat(JS):Added the abillity to paint X or O depending on the players turn

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 let play = document.querySelector('#start-game');
+let cells  = document.querySelectorAll('.cell');
+let turn = true;
+
+function addSymbol(cell,player ,symbol){
+    cell.innerHTML = `<${player}>${symbol}</${player}>`;
+}
+function toggleSymbols(toggle){
+    return toggle ? "X" : "O"
+}
 play.addEventListener('click', () => {
     let div = document.querySelector('#hidden');
     div.classList.add('hidden');
+});
+
+cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+        if (cell.innerHTML == '')
+        {   
+            let player = '';
+            if (turn)
+                player = 'blue';
+            else
+                player = 'red'
+            addSymbol(cell, player,toggleSymbols(turn));
+            turn = !turn;    
+        }
+    });
 });
